e867214664ad641cfadf44dc16b9fce67b8041e5
Mr-K10 <kautilyakatiha10@gmail.com>
2020-07-12 23:09:26 +0530

    Added the gear class with chainring and cog as properties

--- singleResponsibility.java
+++ singleResponsibility.java
@@ -9,10 +9,28 @@ public class singleResponsibility {
   System.out.printf("Ratio for 52 chainring and 11 cog is %1.2f \n",ratio1);
   System.out.printf("Ratio for 30 chainring and 27 cog is %1.2f \n",ratio2);
 
+  gear sGear = new gear(52,11);
+  gear bGear = new gear(30,27);
+
+  System.out.printf("Small gear Ratio %1.2f \n",sGear.ratio());
+  System.out.printf("Big gear Ratio %1.2f \n",bGear.ratio());
+
  }
 
  public static double calRatio(int chainring,int cog){
   return (double)chainring/(double)cog;
  }
-
 }
+
+class gear{
+ private int chainring;
+ private int cog;
+ public gear(int chainring,int cog){
+  this.chainring = chainring;
+  this.cog = cog;
+ }
+ double ratio(){
+  return 1.0*chainring/cog;
+ }
+}
+
